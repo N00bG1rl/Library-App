4bbdc38d39a0bcb2eb97d75c6e8ab953cf383111
N00bG1rl <helen7089@gmail.com>
2020-09-08 13:12:28 +0300

    Started Input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
 console.log('Hello world!')
+
+//Example from: https://www.w3schools.com/howto/howto_js_toggle_class.asp
+var element = document.getElementById("myDIV");
+
+if (element.classList) {
+  element.classList.toggle("mystyle");
+} else {
+  // For IE9
+  var classes = element.className.split(" ");
+  var i = classes.indexOf("mystyle");
+
+  if (i >= 0)
+    classes.splice(i, 1);
+  else
+    classes.push("mystyle");
+    element.className = classes.join(" ");
+}
