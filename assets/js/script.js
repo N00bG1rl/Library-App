89809240243789f20666646aa061aa8e88aa3e72
N00bG1rl <helen7089@gmail.com>
2020-11-12 12:39:50 +0200

    Input field

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,18 @@
 /* Overlay appearance */
 let openBtn = document.getElementById("openBtn");
 let closeBtn = document.getElementById("closeBtn");
-let overlay = document.getElementById("overlay")
+let overlay = document.getElementById("overlay");
+let body = document.getElementsByTagName('body')[0];
 
 openBtn.onclick = function() {
   overlay.style.display = "block";
+  body.style.overflow = "hidden";
   closeBtn.classList.add("closeBtnActive");
   openBtn.classList.remove("openBtnActive");
 }
 closeBtn.onclick = function() {
   overlay.style.display = "none";
+  body.style.overflow = "auto";
   openBtn.classList.add("openBtnActive");
   closeBtn.classList.remove("closeBtnActive")
 }
