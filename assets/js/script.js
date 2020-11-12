fa4cc0e8e1c16faa2568c5913c290e2ed19dbc03
N00bG1rl <helen7089@gmail.com>
2020-11-12 16:59:38 +0200

    Content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,22 @@
 /* Overlay appearance */
-let openBtn = document.getElementById("openBtn");
-let closeBtn = document.getElementById("closeBtn");
-let overlay = document.getElementById("overlay");
+let openBtn = document.getElementById('openBtn');
+let closeBtn = document.getElementById('closeBtn');
+let overlay = document.getElementById('overlay');
 let body = document.getElementsByTagName('body')[0];
 
 openBtn.onclick = function() {
-  overlay.style.display = "block";
-  body.style.overflow = "hidden";
-  closeBtn.classList.add("closeBtnActive");
-  openBtn.classList.remove("openBtnActive");
+  overlay.style.display = 'block';
+  body.style.overflow = 'hidden';
+  closeBtn.classList.add('closeBtnActive');
+  openBtn.classList.remove('openBtnActive');
 }
 closeBtn.onclick = function() {
-  overlay.style.display = "none";
-  body.style.overflow = "auto";
-  openBtn.classList.add("openBtnActive");
-  closeBtn.classList.remove("closeBtnActive")
+  overlay.style.display = 'none';
+  body.style.overflow = 'auto';
+  openBtn.classList.add('openBtnActive');
+  closeBtn.classList.remove('closeBtnActive')
 }
+
 
 /* Input field label animation */
 $('input').on('focusin', function() {
@@ -27,3 +28,14 @@ $('input').on('focusout', function() {
     $(this).parent().find('label').removeClass('active');
   }
 });
+
+function addBook() {
+  let title = document.getElementById('title').value;
+
+  if (title) {
+    console.log(title);
+  }
+}
+
+/* If there is any text inside the input field, Add items to library */
+document.getElementById('add').addEventListener('click', addBook);
