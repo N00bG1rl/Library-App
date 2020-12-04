3aefd3a961ffeaa74255b0c61706b61e8993533a
N00bG1rl <helen7089@gmail.com>
2020-12-04 15:09:41 +0200

    Second part JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,13 +29,57 @@ closeBtn.onclick = function() {
 };
 
 /* BOOK LIBRARY */
-let data = {/* (localStorage.getItem('bookList')) ? JSON.parse(localStorage.getItem('bookList')):{ */
+let data = (localStorage.getItem('bookList')) ? JSON.parse(localStorage.getItem('bookList')):{
   unread: [],
   readed: []
 };
 
 let completeSVG = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="48px" height="48px"><path class="fill" d="M 22.59375 3.5 L 8.0625 18.1875 L 1.40625 11.5625 L 0 13 L 8.0625 21 L 24 4.9375 Z"/></svg>';
 let removeSVG = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="48px" height="48px"><path class="fill" d="M 10.3125 -0.03125 C 8.589844 -0.03125 7.164063 1.316406 7 3 L 2 3 L 2 5 L 6.96875 5 L 6.96875 5.03125 L 17.03125 5.03125 L 17.03125 5 L 22 5 L 22 3 L 17 3 C 16.84375 1.316406 15.484375 -0.03125 13.8125 -0.03125 Z M 10.3125 2.03125 L 13.8125 2.03125 C 14.320313 2.03125 14.695313 2.429688 14.84375 2.96875 L 9.15625 2.96875 C 9.296875 2.429688 9.6875 2.03125 10.3125 2.03125 Z M 4 6 L 4 22.5 C 4 23.300781 4.699219 24 5.5 24 L 18.59375 24 C 19.394531 24 20.09375 23.300781 20.09375 22.5 L 20.09375 6 Z M 7 9 L 8 9 L 8 22 L 7 22 Z M 10 9 L 11 9 L 11 22 L 10 22 Z M 13 9 L 14 9 L 14 22 L 13 22 Z M 16 9 L 17 9 L 17 22 L 16 22 Z"/></svg>';
+
+renderBookList();
+
+document.getElementById('add').addEventListener('click', function() {
+  let title = document.getElementById('title').value;
+  let author = document.getElementById('author').value;
+  let pages = document.getElementById('pages').value;
+  let element = title + author + pages;
+
+  if (title && author && pages) {
+    addItemtoDOM(title, author, pages);
+
+    /* Resets input field */
+    document.getElementById('title').value = "";
+    document.getElementById('author').value = "";
+    document.getElementById('pages').value = "";
+
+    /* Close overlay after submit/add button is clicked */
+    overlay.style.display = 'none';
+
+    /* Store content into data array for localhost */
+    data.unread.push(title, author, pages);
+    dataObjectUpdated();
+  }
+});
+
+function renderBookList() {
+  if (!data.unread.length && !data.readed.length) return;
+
+  for (var i=0; i<data.unread.length; i++) {
+    /* let value = data.unread[i]; */
+    addItemtoDOM(title, author, pages);
+  }
+
+  for (var j=0; j<data.readed.length; j++) {
+    let value = data.readed[j];
+    addItemtoDOM(value, true);
+  }
+}
+
+/* Update local storage */
+function dataObjectUpdated() {
+  localStorage.setItem('bookList', JSON.stringify(data));
+};
 
 /* Delete book from list */
 function removeItem() {
@@ -44,13 +88,12 @@ function removeItem() {
   let id = parent.id;
   let value = item.innerText;
 
-/*   if (id === 'unreadList') {
+  if (id === 'unreadList') {
     data.unread.splice(data.unread.indexOf(value), 1);
   } else {
     data.readed.splice(data.readed.indexOf(value), 1);
   }
-  dataObjectUpdated(); */
-
+  dataObjectUpdated();
   parent.removeChild(item);
 };
 
@@ -62,14 +105,14 @@ function completeItem() {
   let value = item.innerText;
 
   /* Doggle between readed/unread and push to data array */
-/*   if (id === 'unreadList') {
+  if (id === 'unreadList') {
     data.unread.splice(data.unread.indexOf(value), 1);
     data.readed.push(value);
   } else {
     data.readed.splice(data.readed.indexOf(value), 1);
     data.unread.push(value);
   }
-  dataObjectUpdated(); */
+  dataObjectUpdated();
 
   /* Check if the item should be added to the readed list or to re-added to unread list */
   let target = (id === 'unreadList') ? document.getElementById('readedList'):document.getElementById('unreadList');
@@ -80,31 +123,11 @@ function completeItem() {
   target.insertBefore(item, target.childNodes[0]);
 };
 
-document.getElementById('add').addEventListener('click', function() {
-  let title = document.getElementById('title').value;
-  let author = document.getElementById('author').value;
-  let pages = document.getElementById('pages').value;
-  let element = title + author + pages;
-  if (title && author && pages) {
-    /* Store content into data array for localhost */
-    
-
-    addItemtoDOM(title, author, pages);
-
-    /* Close overlay after submit/add button is clicked */
-    overlay.style.display = 'none';
-
-    /* Resets input field */
-    document.getElementById('title').value = "";
-    document.getElementById('author').value = "";
-    document.getElementById('pages').value = "";
-
-  }
-});
-
-function addItemtoDOM(title, author, pages) {
+function addItemtoDOM(title, author, pages, storage) {
   /* Get unread ul list from html */
-  let list = document.getElementById('unreadList');
+  /* let list = document.getElementById('unreadList'); */
+
+  let list = (storage) ? document.getElementById('readedList'):document.getElementById('unreadList');
 
   let item = document.createElement('li');
    
@@ -136,8 +159,6 @@ function addItemtoDOM(title, author, pages) {
 
   /* Add click event to remove the item */
   remove.addEventListener('click', removeItem);
-
-/* console.log(title); */
 
   content.appendChild(h3);
   content.appendChild(span);
